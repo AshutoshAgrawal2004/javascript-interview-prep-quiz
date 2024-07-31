150fcc9062acba71d851d3138d30e6bc87b518c0
Ashutosh Agrawal <agrawalashutosh635@gmail.com>
2024-07-31 19:24:07 +0530

    Setting up PWA

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,19 @@ import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./store/store.ts";
 import ReactGA from "react-ga4";
 import { BrowserRouter as Router } from "react-router-dom";
+// @ts-ignore
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content is available. Reload to update?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("The app is ready to work offline");
+  },
+});
 
 const TRACKING_ID = "G-JJBPF5FMJ4";
 ReactGA.initialize(TRACKING_ID);
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from "path"
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "JavaScript Interview Preparation Quiz",
+        short_name: "JS Quiz",
+        description:
+          "A fun quiz to test your JavaScript knowledge and help you prepare for javascript interviews.",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#020817",
+        theme_color: "#020817",
+        icons: [
+          {
+            src: "./maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "./android-chrome-192x192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "./android-chrome-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+      },
+      includeAssets: [
+        "./favicon.ico",
+        "./apple-touch-icon.png",
+        "./android-chrome-192x192.png",
+        "./android-chrome-512x512.png",
+        "./maskable.png",
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
